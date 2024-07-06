7ca1acec6c46d0b19f31cf32f84f819bb72f45e0
[Chrystian Almeida] <[Chrystian.almeida@alunoescolassociais.org.br]>
2024-07-06 18:16:10 -0300

    Função para armazenamentos de conteúdo em array

--- controller.js
+++ controller.js
@@ -1,13 +1,42 @@
 // FUNCAO PARA VALIDACÃO DE ACESSO
 
 function acessar() {
-    let loginEmail = document.getElementById(  'loginEmail').value;
-    let loginSenha = document.getElementById(  'loginSenha').value;
-    
-    if(!loginEmail || !loginSenha) {
+    let loginEmail = document.getElementById('loginEmail').value;
+    let loginSenha = document.getElementById('loginSenha').value;
+
+    if (!loginEmail || !loginSenha) {
         alert("Ta errado jumento.");
-    }else{
-        alert("Preencheu os campo certo lenda");
+    } else {
+        window.location.href = "cadastro.html";
     }
 
 }
+
+// FUNÇÃO PARA ARMAZENAMENTO DE NOMES EM ARRAY
+
+var dadosLista = [];
+// ESCUTADOR ADICIONADO
+document.getElementById('form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    const nomeUser = document.getElementById('nomeUsuario').value;
+    if (nomeUser) {
+        dadosLista.push(nomeUser);
+
+        document.getElementById("users").innerHTML =
+        dadosLista.map(usernames => 
+            `<tr>
+            <td>${usernames}</td>
+            <td>Inserido</td>
+            </tr>`
+        ).join("");
+
+    } else {
+        console.log("Favor informe nome para cadastro!");
+    }
+}
+);
+
+
+
+
+
