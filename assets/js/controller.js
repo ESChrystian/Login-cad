8bfbeceb328f6d394adbd3753cec5e2d15c87380
Chrystian de Almeida Silva <chrystian.almeida@alunoescolassociais.org.br>
2024-08-16 09:33:45 -0300

    Campo de email adicionado ao cadastro de usuário

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -1,6 +1,5 @@
 // VALIDAR ACESSO EM TELA DE LOGIN
 function acessar() { // CRIA UMA FUNÇÃO  QUE VALIDA O QUE FOI ESCRITO NOS CAMPOS DE LOGIN
-    let loginEmail = document.getElementById("loginEmail").value;
     let loginSenha = document.getElementById("loginSenha").value;
     
     if (document.forms[0].email.value == "" ||
@@ -24,18 +23,21 @@ function acessar() { // CRIA UMA FUNÇÃO  QUE VALIDA O QUE FOI ESCRITO NOS CAMP
 
 
 
-var dadosLista = []; //Array
+var userLista = []; //Array Usuarios
+var emaillista = [];// Array email
 var alert = document.getElementById("alert"); // Mensagem de Span, variavel utilizada nas funcões salvarUser e editar
 var h6 = document.getElementById("text");
 // FUNÇÃO QUE ARMAZENA EM ARRAY NOME NA TELA DE CADASTRO
 function salvarUser() {
     let nomeUser = document.getElementById("nomeUser").value; // Pega valor do input digitado pelo usuário
-
+    let emailUser = document.getElementById("emailUser").value; // Pega valor do input digitado pelo usuário
     if (nomeUser) { //Se existe valor digitado pelo user
 
-        dadosLista.push(nomeUser); // Adiciona valor ao array
+        userLista.push(nomeUser); // Adiciona valor ao array
+        emaillista.push(emailUser);
         criaLista(); // Inicia a funcão cria lista
         document.getElementById("nomeUser").value = ""; // Após envio do campo de Login, o input fica vazio
+        document.getElementById("emailUser").value = ""; // Após envio do campo de Login, o input fica vazio
         alert.innerHTML = ""; //Div onde fica mensagem de aviso fica vazia
         h6.style.display = "none";
 
@@ -51,6 +53,7 @@ function salvarUser() {
 
 }
 
+
 // FUNÇÃO QUE CRIA LISTA DE USUÁRIOS
 
 // <td> = criar a coluna | <tr> = criar a linha |<th> = cabeçalho da tabela| += = oque estiver dentro da tabela fica, mais adiciona mais algum item há tabela.
@@ -58,20 +61,21 @@ function salvarUser() {
 function criaLista() {
     let tabelaDesign = document.getElementById("tabela"); //Variável para personalização da tabela
     // Estrutura da tabela que receberá dados dos usuários
-    let tabela = document.getElementById("tabela").innerHTML = "<tr><th>Nome Usuário</th><th>Ações</th></tr>";
-    for (let i = 0; i <= (dadosLista.length - 1); i++) {
+    let tabela = document.getElementById("tabela").innerHTML = "<tr><th>Nome Usuário</th><th>Email</th><th>Ações</th></tr>";
+    for (let i = 0; i <= (userLista.length - 1); i++) {
         tabelaDesign.style.backgroundColor = "#ffff";
-        tabela += "<tr><td>" + dadosLista[i] + "</td><td><button type='button' onclick='editar(parentNode.parentNode.rowIndex)'>Editar</button><button type='button' onclick='excluir(parentNode.parentNode.rowIndex)'>Excluir</button></td></tr>";
+        tabela += "<tr><td>" + userLista[i] + "</td><td>" + emaillista[i] + "</td><td><button type='button' onclick='editar(parentNode.parentNode.rowIndex)'>Editar</button><button type='button' onclick='excluir(parentNode.parentNode.rowIndex)'>Excluir</button></td></tr>";
         document.getElementById("tabela").innerHTML = tabela;
 
     }
-
+    
 }
 
 // FUNÇÃO PARA EDITAR NOMES DE LISTA
 function editar(i) {
 
     document.getElementById("nomeUser").value = dadosLista[(i - 1)]; dadosLista.splice(dadosLista[(i - 1)], 1);
+    document.getElementById("emailUser").value = emaillista[(i - 1)]; emaillista.splice(dadosLista[(i - 1)], 1);
     alert.innerHTML = "";
 
 }
@@ -79,6 +83,7 @@ function editar(i) {
 // FUNÇÃO QUE EXCLUI NOME DA LISTA
 function excluir(i) {
     dadosLista.splice((i - 1), 1);
+    emaillista.splice((i - 1), 1);
     document.getElementById('tabela').deleteRow(i);
 }
 
